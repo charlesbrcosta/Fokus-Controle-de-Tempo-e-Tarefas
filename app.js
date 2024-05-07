86939c7588fa2b1f9a29fdb746f8a7bf61388d03
Charles Bruno <charlesbrcosta@gmail.com>
2024-05-06 21:14:24 -0300

    exclusão do arquivo teste script.js. adição função mostrarTempo, dos icons de pausa e começar

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 const html = document.querySelector('html');
-
 const buttons = document.querySelectorAll('.app__card-button');
 const startPause = document.getElementById('start-pause');
-
+const iniciarOuPausaBt = document.querySelector('#start-pause span'); 
 const banners = document.querySelector('.app__image');
-
 const title = document.querySelector('.app__title');
-
 const checkbox = document.querySelector('input[type=checkbox]');
-
 const musicFocoInput = document.getElementById('alternar-musica');
+const iconComeçarOuPausar = document.querySelector('.app__card-primary-butto-icon');
+const tempoNaTela = document.getElementById('timer');
+
+let tempoDecorridoEmSegundos = 1500;
+let intervaloId = null
 
 //instância de um novo objeto Audio
 const music = new Audio('./sons/luna-rise-part-one.mp3');
-
-let tempoDeCorridoEmSegundos = 5;
-let intervaloId = null
+const audioPlay = new Audio('./sons/play.wav');
+const audioPausa = new Audio('./sons/pause.mp3');
+const audioTempoFinalizado = new Audio('./sons/beep.mp3');
 
 music.loop = true;
 
@@ -55,6 +56,7 @@ const contextMessages = {
 
 /* Faz alterações nos contextos */
 function handleChangeContext( context ) {
+    mostrarTempo();
     // Verifica se o contexto é valido no objeto contextMessages antes de prosseguir.
     if(!contextMessages.hasOwnProperty( context )) {
         console.error(`Erro: contexto "${context}" inválido`);
@@ -92,13 +94,17 @@ buttons.forEach(button => {
     button.addEventListener('click', () =>{
         
         if(attributeContext === 'foco'){
+            tempoDecorridoEmSegundos = 1500;
             handleChangeContext('foco');
             
         } else if(attributeContext === 'short'){
+            tempoDecorridoEmSegundos = 300;
             handleChangeContext('descanso-curto');
-
+            
         } else {
+            tempoDecorridoEmSegundos = 900;
             handleChangeContext('descanso-longo');
+            
         }
             
         handleSetbuttonActiveStyle(button);
@@ -107,31 +113,44 @@ buttons.forEach(button => {
 
 const contagemRegressiva = () => {
 
-    if(tempoDeCorridoEmSegundos === 0 ) {
-        console.log('entrou no if do tempoDeCorrido');
+    if(tempoDecorridoEmSegundos === 0 ) {
+        audioTempoFinalizado.play();
+        alert('Tempo finalizado.');
         zerar();
-        console.log('tempo finalizado..');
         return;
     }
-
-    tempoDeCorridoEmSegundos -= 1;
-    console.log(tempoDeCorridoEmSegundos);
+    tempoDecorridoEmSegundos -= 1;
+    mostrarTempo();
 }
 
-startPause.addEventListener('click', inicial);
+startPause.addEventListener('click', iniciarOuPausar);
 
-function inicial() {
+function iniciarOuPausar() {
     if(intervaloId){
+        audioPausa.play();
         zerar();
         return;
     }
+    audioPlay.play();
     intervaloId = setInterval(contagemRegressiva, 1000);
+    iniciarOuPausaBt.textContent = 'Pausar'
+    iconComeçarOuPausar.setAttribute('src', './imagens/pause.png');
 }
 
 function zerar() {
     clearInterval(intervaloId);
+    iniciarOuPausaBt.textContent = 'Começar'
+    iconComeçarOuPausar.setAttribute('src', './imagens/play_arrow.png');
     intervaloId = null;
 }
+
+function mostrarTempo() {
+    const tempo = new Date(tempoDecorridoEmSegundos * 1000);
+    const tempoFormatado = tempo.toLocaleTimeString('pt-br', { minute: '2-digit', second: '2-digit'});
+    tempoNaTela.innerHTML = `${tempoFormatado}`;
+}
+
+mostrarTempo();
 
 window.onload = () => {
     // Garante que o checkbox esteja desmarcado ao carregar ou atualizar a página.
